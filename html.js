df7aaafad64f8289ced43af1abc9c1943b9a70b4
jonayGodoy <jonaydam2@gmail.com>
2017-06-24 15:57:26 +0100

    improve metaDescription

--- html.js
+++ html.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Helmet from "react-helmet"
+import { config } from 'config'
 import { prefixLink } from 'gatsby-helpers'
 import { GoogleFont, TypographyStyle } from 'react-typography'
 import typography from './utils/typography'
 
-const BUILD_TIME = new Date().getTime()
+const BUILD_TIME = new Date().getTime();
 
 module.exports = React.createClass({
   displayName: 'HTML',
@@ -12,10 +13,10 @@ module.exports = React.createClass({
     body: React.PropTypes.string,
   },
   render () {
-    const { body } = this.props
+    const { body } = this.props;
     const head = Helmet.rewind();
 
-    let css
+    let css;
     if (process.env.NODE_ENV === 'production') {
       css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
     }
@@ -30,7 +31,46 @@ module.exports = React.createClass({
             content="width=device-width, initial-scale=1.0"
           />
           {head.title.toComponent()}
-          {head.meta.toComponent()}
+
+            {/*INFO*/}
+          <meta name="Author" content="Jonay Godoy" />
+          <meta name="description" content="Blog personal sobre informática y programación"/>
+          <meta name="keywords" content="jonay godoy, clean code, javascript,"/>
+          <meta name="robots" content="index, follow" />
+            {/*Info
+          <meta name="msvalidate.01" content="453947665841FBB11EFD972217B399EE" />
+          <meta name="Copyright" content="Copyright (c) 2017 Ulises Santana" />
+          */}
+
+            {/*ITEM PROPERTIES*/}
+          <meta itemprop="name" content="Jonay Godoy" />
+          <meta itemprop="jobTitle" content="Developer" />
+          <meta itemprop="description" content={config.blogDescription}/>
+          <meta itemprop="knows" content="javascript, php" />
+          <meta itemprop="email" content="jonay.godoy.reyes@gmail.com" />
+          <meta itemprop="alternateName" content="jonay" />
+          <meta itemprop="follows" content="https://twitter.com/GodoyJonay" />
+
+          
+            {/*THUMB
+          <meta property="og:image" content="img/profile.jpg"/>
+          <link itemprop="image" href="img/profile.jpg"/>
+          <meta name="thumbnail" content="img/profile.jpg" />
+          <meta property="og:image:width" content="460" />
+          <meta property="og:image:height" content="460" />
+          <link rel="canonical" href="https://ulisesantana.github.io/"/>
+          <meta property="og:url" content="https://ulisesantana.github.io/"/>
+          */}
+          <meta property="og:type" content="website"/>
+
+
+            {/*TWITTER*/}
+          <meta name="twitter:site" content="@GodoyJonay"/>
+          <meta name="twitter:card" content="summary"/>
+          {/*<meta name="twitter:image" content="https://ulisesantana.github.io/img/profile.jpg" />*/}
+          <meta name="twitter:title" content={config.blogTitle} />
+          <meta name="twitter:description" content={config.blogDescription}/>
+
           <TypographyStyle typography={typography} />
           <GoogleFont typography={typography} />
           {css}
@@ -42,4 +82,4 @@ module.exports = React.createClass({
       </html>
     )
   },
-})
+});
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router'
 import sortBy from 'lodash/sortBy'
 import get from 'lodash/get'
 import { prefixLink } from 'gatsby-helpers'
@@ -14,19 +13,20 @@ import BoxArticle from '../components/commons/BoxArticle';
 class BlogIndex extends React.Component {
   render () {
     // Sort pages.
-    const sortedPages = sortBy(this.props.route.pages, 'data.date')
+    const sortedPages = sortBy(this.props.route.pages, 'data.date');
     // Posts are those with md extension that are not 404 pages OR have a date (meaning they're a react component post).
     const visiblePages = sortedPages.filter(page => (
       get(page, 'file.ext') === 'md' && !include(page.path, '/404') || get(page, 'data.date')
-    ))
+    ));
     return (
       <div>
         <Helmet
           title={config.blogTitle}
+          /*
           meta={[
             {"name": "description", "content": "Sample blog"},
             {"name": "keywords", "content": "blog, articles"},
-          ]}
+          ]}*/
         />
         <ul>
           {visiblePages.map((page) => (
@@ -40,6 +40,6 @@ class BlogIndex extends React.Component {
 
 BlogIndex.propTypes = {
   route: React.PropTypes.object,
-}
+};
 
 export default BlogIndex
