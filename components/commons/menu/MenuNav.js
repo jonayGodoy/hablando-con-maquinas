c39c5aae4a649d9088fe28a1872703f80d886da3
jonayGodoy <jonaydam2@gmail.com>
2017-06-24 16:33:28 +0100

    add title to head

--- components/commons/menu/MenuNav.js
+++ components/commons/menu/MenuNav.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import { Link } from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
+import { config } from 'config'
 
 import '../../../css/menuNav.css';
 import img_head from '../../../pages/global-img/head.png';
@@ -13,7 +14,8 @@ const MenuNav = () => {
                     <Link to={prefixLink('/')}>
                         <img className="col-xs-5 col-md-5 img_logo img-responsive " src={img_head}/>
                     </Link>
-                    <nav className="blog-nav col-md-offset-1 col-md-6">
+                    <nav className=" blog-nav col-md-offset-1 col-md-6">
+                        <h3 className="text-center" style={{paddingBottom: "1em"}}>{config.blogTitle}</h3>
                         <Link className="blog-nav-item" to={prefixLink('/')}>Inicio</Link>
                     </nav>
                 </div>
@@ -22,6 +24,5 @@ const MenuNav = () => {
         </div>
     );
 };
-
 
 export default MenuNav;
