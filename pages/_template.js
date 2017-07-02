b630febb04b2cbc5a64c43cbdb132df00f5b78e8
jonayGodoy <jonaydam2@gmail.com>
2017-07-02 09:59:30 +0100

    sort components

--- pages/_template.js
+++ pages/_template.js
@@ -4,8 +4,7 @@ import { prefixLink } from 'gatsby-helpers';
 //import { rhythm, scale } from 'utils/typography';
 import { config } from 'config';
 
-import MenuNav from 'src/components/commons/menu/MenuNav';
-import MenuMobile from 'src/components/commons/menu/MenuMobile/MenuMobile';
+import MainTemplate from 'src/components/commons/template/MainTemplate';
 
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -14,24 +13,8 @@ import '../css/blog.css';
 
 class Template extends React.Component {
   render () {
-    const { location, children } = this.props;
-
-    return (
-        <div className="container-fluid">
-            <div id="menu_mobile">
-                <MenuMobile/>
-            </div>
-            <div className="container-fluid blog-main" >
-                <MenuNav />
-                <div id="pageMain" className="row">
-                    {/* Main Content */}
-                        {children}
-                </div>
-            </div>
-            { /* Footer -->
-             partial('_partial/footer')  */}
-        </div>
-    )
+      const { location, children } = this.props;
+      return <MainTemplate children={children}/>
   }
 }
 
---
+++ src/components/commons/template/MainTemplate.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { prefixLink } from 'gatsby-helpers';
+
+//import { rhythm, scale } from 'utils/typography';
+import { config } from 'config';
+
+import MenuNav from '../../commons/menu/MenuNav';
+import MenuMobile from '../../commons/menu/MenuMobile/MenuMobile';
+
+
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../../../css/blog.css';
+
+
+class MainTemplate extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render () {
+        return (
+            <div className="container-fluid">
+                <div id="menu_mobile">
+                    <MenuMobile/>
+                </div>
+                <div className="container-fluid blog-main" >
+                    <MenuNav />
+                    <div id="pageMain" className="row">
+                        {/* Main Content */}
+                        {this.props.children}
+                    </div>
+                </div>
+                { /* Footer -->
+                 partial('_partial/footer')  */}
+            </div>
+        )
+    }
+}
+
+MainTemplate.propTypes = {
+    children: React.PropTypes.any,
+    location: React.PropTypes.object,
+    route: React.PropTypes.object
+};
+
+export default MainTemplate;
