82e5ad8a257e4f37b154edbb196a79c2bca95d1b
JonayGodoy <jonay.godoy.reyes@gmail.com>
2017-11-16 15:51:50 +0000

    refactor clean Cell

--- src/components/gameOfLife/Cell.js
+++ src/components/gameOfLife/Cell.js
@@ -1,13 +1,4 @@
-let Cell = (world, coordinate) => {
-    let self = {
-        update: () =>{
-            let numberCellsNeighbours = world.getNumberCellsNeighbours(coordinate);
-            if(numberCellsNeighbours < 2){self.die()}
-            if(numberCellsNeighbours > 3){self.die()}
-        },
-        die: () =>{world.killsCell(coordinate)}
-    };
-    return self;
-
+let Cell = () => {
+    return {};
 };
 export default Cell;
