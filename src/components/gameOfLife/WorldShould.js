205e9e9d54942e6af9d055052ebec315af10c83a
JonayGodoy <jonay.godoy.reyes@gmail.com>
2017-11-16 13:09:57 +0000

    refactor test extract field world

--- src/components/gameOfLife/WorldShould.js
+++ src/components/gameOfLife/WorldShould.js
@@ -1,23 +1,28 @@
+import {beforeEach} from "mocha";
 import { assert } from 'chai';
 import World from './World';
 import Coordinate from './Coordinate';
 
 describe("World Should",() => {
+
+    let world;
+
+    beforeEach(function () {
+        world =  World();
+    });
+
     it("Cell not live in 0-0", function () {
-      let world =  World();
       let isLive = world.isLiveCellInCoordinate(Coordinate(0,0));
       assert.equal(isLive, false);
     });
 
     it("Cell live in 0-0", function () {
-        let world =  World();
         world.createCell(Coordinate(0,0));
         let isLive = world.isLiveCellInCoordinate(Coordinate(0,0));
         assert.equal(isLive, true);
     });
 
     it("Cell has 8 neighbour", function () {
-        let world =  World();
         world.createCell(Coordinate(0,0));
 
         world.createCell(Coordinate(0,-1));
@@ -34,9 +39,9 @@ describe("World Should",() => {
     });
 
     it("when Cell has less 2 neighbour die", function () {
-        let world =  World();
         world.createCell(Coordinate(0,0));
         world.createCell(Coordinate(0,-1));
+
         world.update();
 
         assert.equal(world.isLiveCellInCoordinate(Coordinate(0,0)), false);
@@ -44,15 +49,17 @@ describe("World Should",() => {
     });
 
     it("when Cell has 2 or 3 survive", function () {
-        let world =  World();
         world.createCell(Coordinate(0,0));
         world.createCell(Coordinate(0,1));
+        world.createCell(Coordinate(0,2));
         world.createCell(Coordinate(1,1));
         world.createCell(Coordinate(1,0));
+
         world.update();
 
         assert.equal(world.isLiveCellInCoordinate(Coordinate(0,0)), true);
         assert.equal(world.isLiveCellInCoordinate(Coordinate(0,1)), true);
+        assert.equal(world.isLiveCellInCoordinate(Coordinate(0,2)), true);
         assert.equal(world.isLiveCellInCoordinate(Coordinate(1,1)), true);
         assert.equal(world.isLiveCellInCoordinate(Coordinate(1,0)), true);
     });
