52df1cedab7805175ecbcd3bbdaef734e32cd5c1
JonayGodoy <jonay.godoy.reyes@gmail.com>
2017-12-09 19:01:59 +0000

    delete useLess dependency

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,4 @@
 import React from "react"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
 
 import MainTemplate from "../components/commons/template/MainTemplate";
 
