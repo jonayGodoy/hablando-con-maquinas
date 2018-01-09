5b2d95d5c3203191ddf92c7d1c3abb3e8717e1e1
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-01-09 06:34:25 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-9fbd0eff722e874f917c.js","60d32d710cba75b017ba590a6c489ad9"],["/commons-bc316eee63409dbca49e.js","dfb978edb2528ed78e5f05c7f48d8e23"],["/index.html","6e4513dd50d031652fff431bd9a2d792"],["/offline-plugin-app-shell-fallback/index.html","813338e1bde0af11c908886cbd5f2e24"]];
+var precacheConfig = [["/app-9fbd0eff722e874f917c.js","60d32d710cba75b017ba590a6c489ad9"],["/commons-bc316eee63409dbca49e.js","dfb978edb2528ed78e5f05c7f48d8e23"],["/index.html","e91e49a47cccaeab0ccde40e0962153b"],["/offline-plugin-app-shell-fallback/index.html","df07e000e0221e177353f84d54ac262b"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
