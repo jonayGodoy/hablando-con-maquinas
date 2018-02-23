6408f01e0ef939275db25c084a61123c58b02605
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-02-23 06:17:33 +0000

    remove boostrap menuNav

--- src/components/commons/menuNav/MenuNav.js
+++ src/components/commons/menuNav/MenuNav.js
@@ -10,24 +10,21 @@ import img_head from '../../../assets/img/head.jpg';
 
 const MenuNav = () => {
     return (
-        <div className="navbar-btn">
-            <div className="hidden visible-sm visible-md visible-lg">
-                <div className="row">
-                    <div>
-                        <Link to={routesPath.inicio.path}>
-                            <img className="col-xs-5 col-md-5 img-logo" src={img_head}/>
-                        </Link>
-                        <nav className="blog-nav col-md-offset-1 col-md-6">
-                            <Link to={routesPath.inicio.path}>
-                                <h3 className="text-center" style={{paddingBottom: "1em"}}>{config.blogTitle}</h3>
-                            </Link>
-                            {routesPath.menu.map((routePath, index) =>(
-                                    <Link key={index} className="blog-nav-item" to={routePath.path}>{routePath.name}</Link>
-                            ))}
-                        </nav>
-                    </div>
+        <div className="header hidden-header">
+            <div>
+                <div className="img-logo" >
+                    <Link to={routesPath.inicio.path}>
+                        <img src={img_head}/>
+                    </Link>
                 </div>
-                <hr className="head" />
+                <div className="blog-nav">
+                    <Link to={routesPath.inicio.path} className="title-header">
+                        <h3>{config.blogTitle}</h3>
+                    </Link>
+                    {routesPath.menu.map((routePath, index) =>(
+                            <Link key={index} className="blog-nav-item" to={routePath.path}>{routePath.name}</Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
