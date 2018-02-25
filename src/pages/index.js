638788d36148b17b810810b5ea100f14f2c10da0
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-02-25 06:49:36 +0000

    #refactor remove duplicity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,25 @@
 import React from "react"
 import get from "lodash/get"
-import Helmet from "react-helmet"
 import BoxArticle from '../components/boxArticle/BoxArticle';
 import { rhythm } from "../utils/typography"
 
-
-import  RecentArticles from "../components/widgets/recentArticles/RecentArticles";
+import TemplateWithSidebar from '../components/commons/template-with-sidebar/TemplateWithSidebarNotComponent'
 
 import '../templates/blog-post.css';
 import '../components/widgets/recentArticles/sidebar.css'
 
 
 class BlogIndex extends  React.Component {
-    constructor(props, context){
-        super(props, context);
-        
-        this.templateWidget = this.templateWidget.bind(this);
-    }
-    
     render () {
         const posts =  get(this, "props.data.allMarkdownRemark.edges");
-
         let bodyHome = posts.map((post,index) => {
             if (post.node.path !== "/404/") {
                 return <BoxArticle key={index} post={post}/>
             }
         });
 
-        return this.templateWidget(bodyHome, posts);
+        return TemplateWithSidebar(bodyHome, posts);
     }
-    
-    templateWidget(body,posts){
-        return (
-            <div className="container-new">
-                <div className="article">
-                    <Helmet title={get(this, "props.data.site.siteMetadata.title")} />
-                    <ul>
-                        {body}
-                    </ul>
-                </div>
-                <div className="sidebar-column">
-                    <div>
-                        <RecentArticles posts={posts}/>
-                    </div>
-                </div>
-                <div className="parchForSizeContainer"/>
-            </div>
-        )
-    }
-
 }
 
 export default BlogIndex
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import get from 'lodash/get'
-import { rhythm, scale } from '../utils/typography'
-import config from "../../config-blog"
+import React from 'react';
+import { rhythm, scale } from '../utils/typography';
+import config from "../../config-blog";
 
 import DisqusComments  from 'react-disqus-comments';
-import  RecentArticles from "../components/widgets/recentArticles/RecentArticles";
+
+import TemplateWithSidebar from '../components/commons/template-with-sidebar/TemplateWithSidebarNotComponent'
 
 import "./md.css";
 import "./blog-post.css"
@@ -14,43 +13,30 @@ const imagesAllArticles = require.context("../pages/", true,  /\.(png|jpg|gif)$/
 
 class BlogPostTemplate extends React.Component {
 
-
     constructor(props, context){
         super(props, context);
         this.post = this.props.data.markdownRemark;
         this.cover = imagesAllArticles("./"+this.post.frontmatter.image_article.relativePath);
     }
 
-
     render () {
         const posts =  this.props.data.allMarkdownRemark.edges;
 
+        let bodyPost = (
+            <div>
+                <img className="img-thumbnail-new" src={this.cover} />
+                <h1>{this.post.frontmatter.title}</h1>
 
-        return (
-            <div className="container-new">
-                <div className="article">
-                    <Helmet title={get(this, "props.data.site.siteMetadata.title")} />
-                    <div>
-                        <img className="img-thumbnail-new" src={this.cover} />
-                        <h1>{this.post.frontmatter.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: this.post.html }} />
+                <hr/>
+                <DisqusComments
+                    shortname={config.disqus_shortname}
+                    identifier={this.post.frontmatter.title}
+                    title={this.post.frontmatter.title}
+                />
+            </div>);
 
-                        <div dangerouslySetInnerHTML={{ __html: this.post.html }} />
-                        <hr/>
-                        <DisqusComments
-                            shortname={config.disqus_shortname}
-                            identifier={this.post.frontmatter.title}
-                            title={this.post.frontmatter.title}
-                        />
-                    </div>
-                </div>
-                <div className="sidebar-column">
-                    <div>
-                        <RecentArticles posts={posts}/>
-                    </div>
-                </div>
-                <div className="parchForSizeContainer"/>
-            </div>
-        )
+        return TemplateWithSidebar(bodyPost,posts);
     }
 
 
