95a30027ff7e05bb4a862dc044811899be13a0d9
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-04-10 05:11:24 +0100

    create footer

--- src/components/commons/template/MainTemplate.js
+++ src/components/commons/template/MainTemplate.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Link from "gatsby-link";
 
 import MenuNav from '../menuNav/MenuNav';
 import MenuMobile from '../menuMobile/MenuMobile';
 
+import config_blog from "../../../../config-blog"
+
 import '../../../styles.css';
 import './template.css';
+import "./footer.css"
+
+import icon_github from "../../../assets/img/footer/github.svg";
+import icon_twitter from "../../../assets/img/footer/twitter.svg";
 
 class MainTemplate extends React.Component {
     constructor(props, context) {
@@ -18,11 +25,26 @@ class MainTemplate extends React.Component {
                 <MenuMobile/>
                 <div className="blog-main" >
                     <MenuNav />
-                    <div id="pageMain">
+                    <div id="pageMain" className="page-main">
                         {/* Main Content */}
                         {this.props.children()}
                     </div>
+                    <footer className="footer">
+                        <p className="title-footer">
+                            {config_blog.blogTitle}
+                        </p>
+                        <div className="icons-footer">
+                            <Link style={{ boxShadow: "none" }} to={"/"}>
+                                <img className="icon-footer" src={icon_twitter} />
+
+                            </Link>
+                            <Link style={{ boxShadow: "none" }} to={"/"}>
+                                <img className="icon-footer" src={icon_github} />
+                            </Link>
+                        </div>
+                    </footer>
                 </div>
+
             </div>
         )
     }
