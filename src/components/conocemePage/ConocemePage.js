5dd910ea3c9d724dbe2e52583071d78ad2eabfa5
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-04-11 06:23:08 +0100

    design base timeline

--- src/components/conocemePage/ConocemePage.js
+++ src/components/conocemePage/ConocemePage.js
@@ -4,10 +4,56 @@ import cv from "./cvDTO";
 import imgCV from '../../assets/img/fot_carnet.jpg';
 
 import './conoceme.css';
+import './conoceme1.css';
+import './timeLine.css'
 
 class ConocemePage extends React.Component {
 
     render () {
+        return(
+            <div>
+                <ul className="timeline">
+
+
+                    <li>
+                        <div className="direction-r">
+                            <div className="flag-wrapper">
+                                <span className="flag">Freelancer</span>
+                                <span className="time-wrapper"><span className="time">2013 - present</span></span>
+                            </div>
+                            <div className="desc">My current employment. Way better than the position before!</div>
+                        </div>
+                    </li>
+
+
+                    <li>
+                        <div className="direction-l">
+                            <div className="flag-wrapper">
+                                <span className="flag">Apple Inc.</span>
+                                <span className="time-wrapper"><span class="time">2011 - 2013</span></span>
+                            </div>
+                            <div className="desc">My first employer. All the stuff I've learned and projects I've been working on.</div>
+                        </div>
+                    </li>
+
+
+                    <li>
+                        <div className="direction-r">
+                            <div className="flag-wrapper">
+                                <span className="flag">Harvard University</span>
+                                <span className="time-wrapper"><span className="time">2008 - 2011</span></span>
+                            </div>
+                            <div className="desc">A description of all the lectures and courses I have taken and my final degree?</div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        );
+
+
+
+
         return (
             <div className="cv">
                 <div>
