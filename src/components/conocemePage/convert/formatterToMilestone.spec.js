ea592aeeb726f1a353a3c9a757c48756b8c86e8c
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-04-16 09:25:17 +0100

    green convert differents awards in milestones. create test that i forget it

--- src/components/conocemePage/convert/formatterToMilestone.spec.js
+++ src/components/conocemePage/convert/formatterToMilestone.spec.js
@@ -100,6 +100,49 @@ describe("convert linkedin entities to milestone",() =>{
         .to.deep.equal(milestone);
     });
 
+    it("convert differents awards in milestones", () =>{
+        let cv = {
+            "education": [
+                {
+                    "institution": "CIFP Villa de Agüimes",
+                    "area": "",
+                    "studyType": "Técnico Superior En Desarrollo De Aplicaciones Multiplataforma",
+                    "startDate": "2013-01-01",
+                    "gpa": "",
+                    "courses": [],
+                    "endDate": "2016-01-01"
+                }],
+            "honors_awards": [
+                {
+                    "Title": "Ganador de segundo puesto en el HackForGood 2017 en Las Palmas con el proyecto Amidi",
+                    "Description": "HackForGood es un evento promovido por Telefónica y organizado junto a la ETSI de Telecomunicación de la Universidad Politécnica de Madrid, la Fundación HazloPosible y la startup MashmeTV con el apoyo de la Red de Cátedras Telefónica en el que Hackers ForGood de toda España proponen retos y desarrollan soluciones para construir un mundo mejor.",
+                    "Issuer": "Fundación Parque Cientifico Tecnologico de la ULPGC",
+                    "Issue_Date": "3/2017"
+                }]
+        };
+
+        let milestone =
+            [
+                {
+                    "title": "Técnico Superior En Desarrollo De Aplicaciones Multiplataforma",
+                    "date":"2013-01-01",
+                    "summary": "",
+                    "description" : ""
+                },
+                {
+                    "title": cv.honors_awards[0].Title,
+                    "date": cv.honors_awards[0].Issue_Date,
+                    "summary": cv.honors_awards[0].Description.substring(0,150)+"...",
+                    "description" : cv.honors_awards[0].Description
+                }
+            ];
+
+        const result = formatter.linkedinToMilestone(cv);
+
+        expect(milestone.length).to.equal(2);
+        expect(milestone).to.deep.equal(result);
+    });
+
     it("return empty when honors_awards is undefined", () =>{
         let honors_awards = {};
         let milestone = [];
