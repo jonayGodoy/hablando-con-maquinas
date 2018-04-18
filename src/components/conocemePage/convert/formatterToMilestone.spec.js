920160da59e2ddc5705b60a52c391e104b99e574
JonayGodoy <jonay.godoy.reyes@gmail.com>
2018-04-18 04:29:39 +0100

    improve test

--- src/components/conocemePage/convert/formatterToMilestone.spec.js
+++ src/components/conocemePage/convert/formatterToMilestone.spec.js
@@ -1,13 +1,8 @@
 import { expect } from "chai";
 import formatter from "./formatterToMilestone";
 
-/*todo
-* usar las propiedades de cv para construir el milestone en los test
-* */
-
 describe("convert linkedin entities to milestone",() =>{
     it("convert work in milestone", () =>{
-        const workSummary = "-Aprender. A través de practicas como mob-programming, libros, cursos e implementando\nfuncionalidades en algunos proyectos siempre buscando hacerlo de le mejor manera posible.\nAsi como metodologias como integración continua y TDD\n-Las tecnologías que utilizamos fueron javascript, react-redux y springboot(java). Asi como\nherramientas complementarias para testear Jest, mocha, junit, selenium.";
         let cv = {
             "work": [
             {
@@ -15,18 +10,19 @@ describe("convert linkedin entities to milestone",() =>{
                 "position": "Alumno Practicas",
                 "website": "",
                 "startDate": "2017-03-01",
-                "summary": workSummary,
+                "summary": "-Aprender. A través de practicas como mob-programming, libros, cursos e implementando\nfuncionalidades en algunos proyectos siempre buscando hacerlo de le mejor manera posible.\nAsi como metodologias como integración continua y TDD\n-Las tecnologías que utilizamos fueron javascript, react-redux y springboot(java). Asi como\nherramientas complementarias para testear Jest, mocha, junit, selenium.",
                 "highlights": [],
                 "endDate": "2017-06-01"
             }]
         };
+
         let milestone =
              [
                 {
-                    "title": "Alumno Practicas",
-                    "date":"2017-03-01",
-                    "summary": workSummary.substring(0,150)+"...",
-                    "description" : workSummary
+                    "title": first(cv.work).position,
+                    "date": first(cv.work).startDate,
+                    "summary": first(cv.work).summary.substring(0,150)+"...",
+                    "description" : first(cv.work).summary
                 }
             ];
 
@@ -58,8 +54,8 @@ describe("convert linkedin entities to milestone",() =>{
     let milestone =
         [
             {
-                "title": "Técnico Superior En Desarrollo De Aplicaciones Multiplataforma",
-                "date":"2013-01-01",
+                "title": first(cv.education).studyType,
+                "date": first(cv.education).startDate,
                 "summary": "",
                 "description" : ""
             }
@@ -92,10 +88,10 @@ describe("convert linkedin entities to milestone",() =>{
     let milestone =
         [
             {
-                "title": cv.honors_awards[0].Title,
-                "date": cv.honors_awards[0].Issue_Date,
-                "summary": cv.honors_awards[0].Description.substring(0,150)+"...",
-                "description" : cv.honors_awards[0].Description
+                "title": first(cv.honors_awards).Title,
+                "date": first(cv.honors_awards).Issue_Date,
+                "summary": first(cv.honors_awards).Description.substring(0,150)+"...",
+                "description" : first(cv.honors_awards).Description
             }
         ];
 
@@ -128,16 +124,16 @@ describe("convert linkedin entities to milestone",() =>{
         let milestone =
             [
                 {
-                    "title": "Técnico Superior En Desarrollo De Aplicaciones Multiplataforma",
-                    "date":"2013-01-01",
+                    "title": first(cv.education).studyType,
+                    "date": first(cv.education).startDate,
                     "summary": "",
                     "description" : ""
                 },
                 {
-                    "title": cv.honors_awards[0].Title,
-                    "date": cv.honors_awards[0].Issue_Date,
-                    "summary": cv.honors_awards[0].Description.substring(0,150)+"...",
-                    "description" : cv.honors_awards[0].Description
+                    "title": first(cv.honors_awards).Title,
+                    "date": first(cv.honors_awards).Issue_Date,
+                    "summary": first(cv.honors_awards).Description.substring(0,150)+"...",
+                    "description" : first(cv.honors_awards).Description
                 }
             ];
 
@@ -155,3 +151,7 @@ describe("convert linkedin entities to milestone",() =>{
             .to.deep.equal(milestone);
     });
 });
+
+function first(array){
+    return array[0];
+}
